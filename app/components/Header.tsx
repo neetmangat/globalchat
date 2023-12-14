bf7278aeb8ddfef519e3bfe918bf8c748ccaed3c
neetmangat <neetmangat@gmail.com>
2023-12-14 15:02:34 -0800

    Initial viewport setup

---
+++ app/components/Header.tsx
@@ -0,0 +1,13 @@
+import Logo from "./Logo";
+
+function Header() {
+  return (
+    <header>
+      <nav>
+        <Logo />
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
---
+++ app/components/Logo.tsx
@@ -0,0 +1,8 @@
+import LogoImage from "@logos/black.svg";
+import Link from "next/link";
+
+function Logo() {
+  return <Link href="/">Go Home</Link>;
+}
+
+export default Logo;
